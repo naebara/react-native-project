6013b70556cc0329594709ac7c5a4ea7e19d45d9
Nae Bara <natanaelbarag@gmail.com>
2023-03-11 22:51:32 +0200

    Add Icons and Active Selected Tab

--- App.tsx
+++ App.tsx
@@ -1,21 +1,61 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View, Text } from 'react-native';
-import HomeScreen from "./screens/HomeScreen";
-import MoviesScreen from "./screens/MoviesScreen";
-import ContactScreen from "./screens/ContactScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "react-native-vector-icons";
+import HomeScreen from './screens/HomeScreen';
+import MoviesScreen from './screens/MoviesScreen';
+import ContactScreen from './screens/ContactScreen';
 
 const Tab = createBottomTabNavigator();
-
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Movies" component={MoviesScreen} />
-                <Tab.Screen name="Contact" component={ContactScreen} />
+            <Tab.Navigator
+                tabBarOptions={{
+                    activeTintColor: '#ff6347',
+                    inactiveTintColor: 'gray',
+                }}
+            >
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={focused ? 'home' : 'home-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Movies"
+                    component={MoviesScreen}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={focused ? 'film' : 'film-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Contact"
+                    component={ContactScreen}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={focused ? 'chatbubbles' : 'chatbubbles-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
