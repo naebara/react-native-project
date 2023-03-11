3cd7626ce834d07ce8e6af2c7d0903296f570042
Nae Bara <natanaelbarag@gmail.com>
2023-03-11 22:40:43 +0200

    Components on the same page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Ce prost esti!</Text>
-      <Text>Ce prost esti!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+};
+
+const MoviesScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Movies Screen</Text>
+        </View>
+    );
+};
+
+const ContactScreen = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Contact Screen</Text>
+        </View>
+    );
+};
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Movies" component={MoviesScreen} />
+                <Tab.Screen name="Contact" component={ContactScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
