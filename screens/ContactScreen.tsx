fe5adc710f7dbb9b197b7c378f3c9c37a7fd39fb
Nae Bara <natanaelbarag@gmail.com>
2023-03-16 00:43:08 +0200

    Merge bine

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -1,13 +1,20 @@
 import {Text, View} from "react-native";
 import React, {useContext} from "react";
 import {ContactContext, ContactProvider} from "../context/ContactContext";
+import { Card } from "react-native-elements";
 const ContactScreen = () => {
     const {contactInfo, setContactInfo} = useContext(ContactContext);
     return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>{contactInfo.toString()}</Text>
-            </View>
-
+        <View style={{ flex: 1 }}>
+            {contactInfo.map((contact, index: number) => (
+                <Card key={index}>
+                    <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
+                    <Card.Divider />
+                    <Text>Email: {contact.email}</Text>
+                    <Text>Phone: {contact.phone}</Text>
+                </Card>
+            ))}
+        </View>
     );
 };
 
