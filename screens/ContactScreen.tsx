b8c678e60149d92493e4b63a61bcb3e11e855358
Nae Bara <natanaelbarag@gmail.com>
2023-03-16 00:49:19 +0200

    Context fara type

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -6,8 +6,21 @@ const ContactScreen = () => {
     const {contactInfo, setContactInfo} = useContext(ContactContext);
     return (
         <View style={{ flex: 1 }}>
-            {contactInfo.map((contact, index: number) => (
+            <View
+                style={{
+                    backgroundColor: "#f2f2f2",
+                    padding: 10,
+                    marginBottom: 10,
+                }}
+            >
+                <Text style={{ fontWeight: "bold" }}>
+                    This is a contact page. The following users are available to help for
+                    any questions:
+                </Text>
+            </View>
+            {contactInfo.map((contact: ContactInfo, index: number) => (
                 <Card key={index}>
+                    <Card.Image source={{uri: 'https://st.depositphotos.com/2010047/2144/i/600/depositphotos_21443135-stock-photo-3d-small-with-a-question.jpg'}}></Card.Image>
                     <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
                     <Card.Divider />
                     <Text>Email: {contact.email}</Text>
