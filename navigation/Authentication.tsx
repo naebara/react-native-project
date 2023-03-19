fa7b40eb055e56bdc8b74b44844b4b8d6accd7c3
Mot Timotei Fabian <73028729+MotTimotei@users.noreply.github.com>
2023-03-19 22:22:05 +0200

    sign up screen removed

--- navigation/Authentication.tsx
+++ navigation/Authentication.tsx
@@ -8,7 +8,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LogInScreen from "../screens/LogInScreen";
-import SignUpScreen from "../screens/SignUpScreen";
 import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
@@ -30,20 +29,6 @@ const Authentication = () => {
           }}
           name="LogIn"
           component={LogInScreen}
-        />
-        <Tab.Screen
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ focused, color, size }) => (
-              <Ionicons
-                name={focused ? "person-add" : "person-add-outline"}
-                size={size}
-                color={color}
-              />
-            ),
-          }}
-          name="SignUp"
-          component={SignUpScreen}
         />
       </Tab.Navigator>
     </NavigationContainer>
--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -2,37 +2,45 @@
 // ContactScreen utilizeaza useContext hook pentru a accesa contactInfo, o lista de obiecte ContactInfo din ContactContext.
 // Lista este apoi afisata utilizand React Native Elements Card component pentru fiecare obiect ContactInfo.
 
-import {Text, View} from "react-native";
-import React, {useContext} from "react";
-import {ContactContext, ContactInfo, ContactProvider} from "../context/ContactContext";
+import { Text, View } from "react-native";
+import React, { useContext } from "react";
+import {
+  ContactContext,
+  ContactInfo,
+  ContactProvider,
+} from "../context/ContactContext";
 import { Card } from "react-native-elements";
 const ContactScreen = () => {
-    const {contactInfo} = useContext(ContactContext);
-    return (
-        <View style={{ flex: 1 }}>
-            <View
-                style={{
-                    backgroundColor: "#f2f2f2",
-                    padding: 10,
-                    marginBottom: 10,
-                }}
-            >
-                <Text style={{ fontWeight: "bold" }}>
-                    This is a contact page. The following users are available to help for
-                    any questions:
-                </Text>
-            </View>
-            {contactInfo.map((contact: ContactInfo, index: number) => (
-                <Card key={index}>
-                    <Card.Image source={{uri: 'https://thumbs.dreamstime.com/b/d-white-man-red-questionmark-computer-generated-image-isolated-68105896.jpg'}}></Card.Image>
-                    <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
-                    <Card.Divider />
-                    <Text>Email: {contact.email}</Text>
-                    <Text>Phone: {contact.phone}</Text>
-                </Card>
-            ))}
-        </View>
-    );
+  const { contactInfo } = useContext(ContactContext);
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: "#f2f2f2",
+          padding: 10,
+          marginBottom: 10,
+        }}
+      >
+        <Text style={{ fontWeight: "bold" }}>
+          This is a contact page. The following users are available to help for
+          any questions:
+        </Text>
+      </View>
+      {contactInfo.map((contact: ContactInfo, index: number) => (
+        <Card key={index}>
+          <Card.Image
+            source={{
+              uri: "https://thumbs.dreamstime.com/b/d-white-man-red-questionmark-computer-generated-image-isolated-68105896.jpg",
+            }}
+          ></Card.Image>
+          <Card.Title>{`${contact.firstName} ${contact.lastName}`}</Card.Title>
+          <Card.Divider />
+          <Text>Email: {contact.email}</Text>
+          <Text>Phone: {contact.phone}</Text>
+        </Card>
+      ))}
+    </View>
+  );
 };
 
 export default ContactScreen;
